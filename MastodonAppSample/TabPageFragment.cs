76d60c331cf61c43faf5eab5d988314ad41b513b
meonal <meonalwater@gmail.com>
2017-05-03 15:48:58 +0900

    Initial commit (xamarin sampleをテンプレートとして使用)

---
+++ MastodonAppSample/TabPageFragment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace MastodonAppSample
+{
+    /// <summary>
+    /// タブページ
+    /// </summary>
+    public class TabPageFragment : Fragment
+    {
+        const string ARG_PAGE = "ARG_PAGE";
+        private int mPage;
+
+        public static TabPageFragment newInstance(int page)
+        {
+            var args = new Bundle();
+            args.PutInt(ARG_PAGE, page);
+            var fragment = new TabPageFragment();
+            fragment.Arguments = args;
+            return fragment;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            mPage = Arguments.GetInt(ARG_PAGE);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.fragment_page, container, false);
+            var textView = (TextView) view;
+            textView.Text = "Fragment #" + mPage;
+            return view;
+        }
+    }
+}
---
+++ MastodonAppSample/TabPagePresenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace MastodonAppSample
+{
+    /// <summary>
+    /// タブページ
+    /// </summary>
+    public class TabPagePresenter
+    {
+        public TabPagePresenter()
+        {
+
+        }
+
+    }
+}
